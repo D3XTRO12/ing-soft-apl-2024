27a2525b9074a93401f63c181a58f3c013d5c868
D3XTRO12 <mirazopablo@gmail.com>
2024-11-11 23:51:25 -0300

    v0.1.3

--- user_ms/app/__init__.py
+++ user_ms/app/__init__.py
@@ -45,9 +45,9 @@ def create_app():
     migrate = Migrate(app)
     migrate.init_app(app, db)
 
-    from app.controllers.user_controllers import user
+    from app.controllers.user_controllers import user, home
     app.register_blueprint(user, url_prefix='/user')
-    #app.register_blueprint(home, url_prefix='')
+    app.register_blueprint(home, url_prefix='')
     #app.register_blueprint(category, url_prefix='/category')
     #app.register_blueprint(vehicle, url_prefix='/vehicle')
 
